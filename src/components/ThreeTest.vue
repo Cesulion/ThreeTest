2b3d2bbe8c929deb8a74467661452599510115f3
Cesulion <50085330+Cesulion@users.noreply.github.com>
2020-06-03 17:22:55 +0200

    Commit 1

---
+++ src/components/ThreeTest.vue
@@ -0,0 +1,48 @@
+<template>
+  <div style="z-index: 10" id="container2"></div>
+</template>
+
+<script>
+  import * as Three from 'three'
+  export default {
+    name: 'ThreeTest',
+    data() {
+      return {
+        camera: null,
+        scene: null,
+        renderer: null,
+        cube: null
+      }
+    },
+    methods: {
+      init: function() {
+        let container = document.getElementById('container2');
+
+        this.camera = new Three.PerspectiveCamera(70, container.clientWidth/container.clientHeight, 0.01, 1000);
+        this.camera.position.z = 1;
+
+        this.scene = new Three.Scene();
+
+        let geometry = new Three.BoxGeometry(1, 1, 1);
+        let material = new Three.MeshBasicMaterial({ color: 0x00ff00 });
+        this.cube = new Three.Mesh(geometry, material);
+        this.scene.add(this.cube);
+
+        this.renderer = new Three.WebGLRenderer({antialias: true});
+        this.renderer.setSize(container.clientWidth, container.clientHeight);
+        container.appendChild(this.renderer.domElement);
+
+      },
+      animate: function() {
+        requestAnimationFrame(this.animate);
+        this.cube.rotation.x += 0.01;
+        this.cube.rotation.y += 0.01;
+        this.renderer.render(this.scene, this.camera);
+      }
+    },
+    mounted() {
+      this.init();
+      this.animate();
+    }
+  }
+</script>
