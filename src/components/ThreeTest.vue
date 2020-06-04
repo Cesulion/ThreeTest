0d5e4cb060113fd629b6813ce61750b1d2da1523
Tazen Meriem <meriem.tazen@gmail.com>
2020-06-04 11:05:10 +0200

    container instead of document body

--- src/components/ThreeTest.vue
+++ src/components/ThreeTest.vue
@@ -3,60 +3,59 @@
 </template>
 
 <script>
-export default {
-  name: "ThreeTest",
-
-  data: () => ({}),
-};
-
-// import * as Three from "three";
 // export default {
 //   name: "ThreeTest",
-//   data() {
-//     return {
-//       camera: null,
-//       scene: null,
-//       renderer: null,
-//       cube: null,
-//       targetElement: null,
-//     };
-//   },
-//   methods: {
-//     init: function() {
-//       this.scene = new Three.Scene();
 
-//       let container = document.getElementById("container2");
-
-//       this.camera = new Three.PerspectiveCamera(
-//         70,
-//         container.clientWidth / container.clientHeight,
-//         0.01,
-//         1000
-//       );
-//       this.camera.position.z = 1;
-
-//       let geometry = new Three.BoxGeometry(1, 1, 1);
-//       let material = new Three.MeshBasicMaterial({ color: 0x00ff00 });
-//       this.cube = new Three.Mesh(geometry, material);
-//       this.scene.add(this.cube);
-
-//       this.renderer = new Three.WebGLRenderer({ antialias: true });
-//       this.renderer.setSize(container.clientWidth, container.clientHeight);
-//       document.body.appendChild(this.renderer.domElement);
-//     },
-//     animate: function() {
-//       requestAnimationFrame(this.animate);
-//       this.cube.rotation.x += 0.01;
-//       this.cube.rotation.y += 0.01;
-//       this.renderer.render(this.scene, this.camera);
-//     },
-//   },
-//   mounted() {
-//     this.init();
-//     this.animate();
-//   },
+//   data: () => ({}),
 // };
-//
+
+import * as Three from "three";
+export default {
+  name: "ThreeTest",
+  data() {
+    return {
+      camera: null,
+      scene: null,
+      renderer: null,
+      cube: null,
+      targetElement: null,
+    };
+  },
+  methods: {
+    init: function() {
+      this.scene = new Three.Scene();
+
+      let container = document.getElementById("container2");
+
+      this.camera = new Three.PerspectiveCamera(
+        70,
+        container.clientWidth / container.clientHeight,
+        0.01,
+        1000
+      );
+      this.camera.position.z = 1;
+
+      let geometry = new Three.BoxGeometry(1, 1, 1);
+      let material = new Three.MeshBasicMaterial({ color: 0x00ff00 });
+      this.cube = new Three.Mesh(geometry, material);
+      this.scene.add(this.cube);
+
+      this.renderer = new Three.WebGLRenderer({ antialias: true });
+      this.renderer.setSize(container.clientWidth, container.clientHeight);
+      container.appendChild(this.renderer.domElement);
+    },
+    animate: function() {
+      requestAnimationFrame(this.animate);
+      this.cube.rotation.x += 0.01;
+      this.cube.rotation.y += 0.01;
+      this.renderer.render(this.scene, this.camera);
+    },
+  },
+  mounted() {
+    this.init();
+    this.animate();
+  },
+};
 </script>
 
 <style>
