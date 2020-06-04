f416810c4d6f8c60df95c1e7d9a6b0ffee9d8581
Tazen Meriem <meriem.tazen@gmail.com>
2020-06-04 11:02:08 +0200

    v content missing

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-            v-model="drawer"
-            app
-            clipped
-    >
+    <v-navigation-drawer v-model="drawer" app clipped>
       <v-list dense>
         <v-list-item link>
           <v-list-item-action>
@@ -25,15 +21,15 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar
-            app
-            clipped-left
-    >
+    <v-app-bar app clipped-left>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Application</v-toolbar-title>
     </v-app-bar>
 
-    <ThreeTest />
+    <v-content>
+      <HelloWorld />
+      <ThreeTest />
+    </v-content>
 
     <v-footer app>
       <span>&copy; 2020</span>
@@ -42,20 +38,23 @@
 </template>
 
 <script>
-  import ThreeTest from "./components/ThreeTest";
-  export default {
-    name: 'App',
-    components: {
-      ThreeTest,
-    },
-    props: {
-      source: String,
-    },
-    data: () => ({
-      drawer: null,
-    }),
-    created () {
-      this.$vuetify.theme.dark = true
-    },
-  }
+import ThreeTest from "./components/ThreeTest";
+// import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    ThreeTest,
+    // HelloWorld,
+  },
+  props: {
+    source: String,
+  },
+  data: () => ({
+    drawer: null,
+  }),
+  created() {
+    this.$vuetify.theme.dark = true;
+  },
+};
 </script>
--- src/components/ThreeTest.vue
+++ src/components/ThreeTest.vue
@@ -1,48 +1,67 @@
 <template>
-  <div style="z-index: 10" id="container2"></div>
+  <div class="test" id="container2">hellooo</div>
 </template>
 
 <script>
-  import * as Three from 'three'
-  export default {
-    name: 'ThreeTest',
-    data() {
-      return {
-        camera: null,
-        scene: null,
-        renderer: null,
-        cube: null
-      }
-    },
-    methods: {
-      init: function() {
-        let container = document.getElementById('container2');
+export default {
+  name: "ThreeTest",
 
-        this.camera = new Three.PerspectiveCamera(70, container.clientWidth/container.clientHeight, 0.01, 1000);
-        this.camera.position.z = 1;
+  data: () => ({}),
+};
 
-        this.scene = new Three.Scene();
+// import * as Three from "three";
+// export default {
+//   name: "ThreeTest",
+//   data() {
+//     return {
+//       camera: null,
+//       scene: null,
+//       renderer: null,
+//       cube: null,
+//       targetElement: null,
+//     };
+//   },
+//   methods: {
+//     init: function() {
+//       this.scene = new Three.Scene();
 
-        let geometry = new Three.BoxGeometry(1, 1, 1);
-        let material = new Three.MeshBasicMaterial({ color: 0x00ff00 });
-        this.cube = new Three.Mesh(geometry, material);
-        this.scene.add(this.cube);
+//       let container = document.getElementById("container2");
 
-        this.renderer = new Three.WebGLRenderer({antialias: true});
-        this.renderer.setSize(container.clientWidth, container.clientHeight);
-        container.appendChild(this.renderer.domElement);
+//       this.camera = new Three.PerspectiveCamera(
+//         70,
+//         container.clientWidth / container.clientHeight,
+//         0.01,
+//         1000
+//       );
+//       this.camera.position.z = 1;
 
-      },
-      animate: function() {
-        requestAnimationFrame(this.animate);
-        this.cube.rotation.x += 0.01;
-        this.cube.rotation.y += 0.01;
-        this.renderer.render(this.scene, this.camera);
-      }
-    },
-    mounted() {
-      this.init();
-      this.animate();
-    }
-  }
+//       let geometry = new Three.BoxGeometry(1, 1, 1);
+//       let material = new Three.MeshBasicMaterial({ color: 0x00ff00 });
+//       this.cube = new Three.Mesh(geometry, material);
+//       this.scene.add(this.cube);
+
+//       this.renderer = new Three.WebGLRenderer({ antialias: true });
+//       this.renderer.setSize(container.clientWidth, container.clientHeight);
+//       document.body.appendChild(this.renderer.domElement);
+//     },
+//     animate: function() {
+//       requestAnimationFrame(this.animate);
+//       this.cube.rotation.x += 0.01;
+//       this.cube.rotation.y += 0.01;
+//       this.renderer.render(this.scene, this.camera);
+//     },
+//   },
+//   mounted() {
+//     this.init();
+//     this.animate();
+//   },
+// };
+//
 </script>
+
+<style>
+.test {
+  background-color: rgb(61, 3, 3);
+  height: 100%;
+}
+</style>
